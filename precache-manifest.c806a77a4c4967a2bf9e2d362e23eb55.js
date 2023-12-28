96b3f847580a6cd8f75cc33778127bc389f16045
Ahmed-Sallam22 <ahmed30sallam@gmail.com>
2023-12-28 20:27:27 +0200

    Updates

---
+++ precache-manifest.c806a77a4c4967a2bf9e2d362e23eb55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e940e520779182e89b18b5983832c5",
+    "url": "/Real-Deal/index.html"
+  },
+  {
+    "revision": "3ed8e79620a5b93c4c8b",
+    "url": "/Real-Deal/static/css/2.caec4d16.chunk.css"
+  },
+  {
+    "revision": "3ed8e79620a5b93c4c8b",
+    "url": "/Real-Deal/static/js/2.887556fc.chunk.js"
+  },
+  {
+    "revision": "cadd3141005d4622da7ed1198e0fb875",
+    "url": "/Real-Deal/static/js/2.887556fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be4d86824daa48844ca",
+    "url": "/Real-Deal/static/js/main.d597fb9d.chunk.js"
+  },
+  {
+    "revision": "2a6a661d02210f624d31",
+    "url": "/Real-Deal/static/js/runtime-main.90ad5a60.js"
+  },
+  {
+    "revision": "9cff01e69e2254b0846de3451d410d3e",
+    "url": "/Real-Deal/static/media/1.9cff01e6.jpg"
+  },
+  {
+    "revision": "fb987cacb4a4c1827607dddc9b2f6402",
+    "url": "/Real-Deal/static/media/2.fb987cac.png"
+  },
+  {
+    "revision": "e7538150ec3e146431baccaeb7157bc8",
+    "url": "/Real-Deal/static/media/3.e7538150.png"
+  }
+]);
